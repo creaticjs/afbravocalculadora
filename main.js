2be068e8a6d6c7639705f6449c99f265e0451ff9
Andres <andres_f5@hotmail.com>
2019-03-12 22:34:04 -0500

    calculadora basica

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function getnumber(numero){
-
     var parcial=document.getElementById("output").value;
     numero=String(numero);
     parcial=String(parcial);
@@ -9,28 +8,21 @@ function getnumber(numero){
     if(parcial == 0){
         var res = numero;
     }
-
     document.getElementById("output").value = res; 
-    
 }
-
 function clearout(){
     var res=0;
     document.getElementById("output").value = res; 
     document.getElementById("dato").value = "";
     document.getElementById("operacion").value = "";      
 }
-
 function operador(operacion){
     var memoria=document.getElementById("output").value;
     document.getElementById("output").value = 0; 
 
     document.getElementById("dato").value = memoria;
     document.getElementById("operacion").value = operacion;  
-    console.log(memoria);
-    console.log(operacion);
-
-}
+    }
 
 function operar(){
 
@@ -38,41 +30,19 @@ function operar(){
     var datouno = document.getElementById("dato").value;
     var datodos = document.getElementById("output").value;
     operacion=String(operacion);
-    console.log(datouno);
     datouno=Number(datouno);
     datodos=Number(datodos);
-    
-    console.log(operacion)
-    
-    if(operacion=="suma"){
-        console.log(datouno);
-        console.log(datodos);
-
-        var resultado = datouno+datodos;
+     if(operacion=="suma"){
+       var resultado = datouno+datodos;
     }
     if(operacion=="resta"){
-        console.log(datouno);
-        console.log(datodos);
-
-        var resultado = datouno-datodos;
+       var resultado = datouno-datodos;
     }
     if(operacion=="division"){
-        console.log(datouno);
-        console.log(datodos);
-
-        var resultado = datouno/datodos;
+       var resultado = datouno/datodos;
     }
     if(operacion=="multiplicacion"){
-        console.log(datouno);
-        console.log(datodos);
-
-        var resultado = datouno*datodos;
+       var resultado = datouno*datodos;
     }
-
-    console.log(resultado);
     document.getElementById("output").value = resultado;
-
-
-
 }
-
